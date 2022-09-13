280ab6e553a9be7ef02f78c567d27186f8815c6a
QuaY <2310236318@qq.com>
2022-09-13 16:43:03 +0800

    去除连接测试按钮

--- app/main.py
+++ app/main.py
@@ -30,8 +30,6 @@ class Main:
         # 比如 self.ui.button , self.ui.textEdit
         self.ui = QUiLoader().load(qfile_main)
 
-        self.ui.Button_connect_test.clicked.connect(self.database_connect_test)
-
         self.ui.Button_get_dbnames.clicked.connect(self.get_dbname)
 
     def get_dbname(self):
@@ -66,36 +64,6 @@ class Main:
         # 将数据库名添加到下拉框中
         for dbname in dbnames:
             self.ui.comboBox_databases.addItem(dbname[0])
-
-    def database_connect_test(self):
-        """
-        数据库连接测试
-        """
-        dialect = self.ui.comboBox_dbtype.currentText()
-        if len(host := self.ui.lineEdit_host.text()) == 0:
-            QMessageBox.information(self.ui, '提示', '请填写主机')
-            return
-        if len(port := self.ui.lineEdit_port.text()) == 0:
-            QMessageBox.information(self.ui, '提示', '请填写数据库端口')
-            return
-        if len(username := self.ui.lineEdit_user.text()) == 0:
-            QMessageBox.information(self.ui, '提示', '请填写账号')
-            return
-        if len(password := self.ui.lineEdit_password.text()) == 0:
-            QMessageBox.information(self.ui, '提示', '请填写密码')
-            return
-        if len(database := self.ui.comboBox_databases.currentText()) == 0:
-            QMessageBox.information(self.ui, '提示', '请选择数据库名')
-            return
-
-        res = connection_check(dialect, username, password, host, port, database)
-        if res.get('code'):
-            QMessageBox.information(self.ui, '提示', '数据库连接成功!')
-        else:
-            QMessageBox.critical(self.ui, '错误', '数据库连接失败!')
-
-        # databases = check_sql_link(dialect, username, password, host, port, database)
-        # print(databases)
 
 
 app = QApplication([])
