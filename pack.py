b4cca01af1064f797f3ebf9a6923215f39afcca9
成凯 <2223209623@qq.com>
2023-02-13 17:31:20 +0800

    打包

---
+++ pack.py
@@ -0,0 +1,18 @@
+# !/user/bin/env python
+# coding=utf-8
+# @project : Flask_RESTfulAPI_Codegen_Application
+# @author  : ChengKai
+# @file   : pack.py
+# @ide    : PyCharm
+# @time   : 2023-02-13 17:28:31
+'''
+this is function description
+'''
+# import module your need
+import os
+# nuitka打包
+cmd = 'python -m nuitka --follow-imports --standalone --show-progress --enable-plugin=pyside6  --windows-icon-from-ico=E:\MyProject\StudySource\git_pro\ncepu_huadian\api\icon.ico --include-package=sqlalchemy  start.py'
+
+cmd2 = ''
+
+os.popen()
