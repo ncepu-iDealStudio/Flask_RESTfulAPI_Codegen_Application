3fafd1d9eae4804bfa93342a5080a3546817eba3
成凯 <2223209623@qq.com>
2023-02-24 12:04:04 +0800

    no message

--- pack.py
+++ pack.py
@@ -15,7 +15,7 @@ import os
 
 def nuitaka_pack():
     # nuitka打包
-    cmd = 'python -m nuitka --follow-imports --standalone --show-progress --enable-plugin=pyside6 --windows-disable-console --windows-icon-from-ico=icon.ico --include-package=sqlalchemy  start.py'
+    cmd = 'python -m nuitka --follow-imports --standalone --show-progress --enable-plugin=pyside6 --windows-icon-from-ico=icon.ico --include-package=sqlalchemy  start.py'
     os.system(cmd)
 
 
