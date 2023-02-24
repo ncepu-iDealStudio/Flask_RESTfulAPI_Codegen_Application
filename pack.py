9cd19a9fba5f66a8af6e1892be38985f6c67f3f1
jackiex <xjg@ncepu.edu.cn>
2023-02-24 11:16:02 +0800

    修订打包工具程序

--- pack.py
+++ pack.py
@@ -5,19 +5,34 @@
 # @file   : pack.py
 # @ide    : PyCharm
 # @time   : 2023-02-13 17:28:31
+
 '''
-this is function description
+打包以及功能程序  
 '''
-# import module your need
+
 import os
-# nuitka打包
-cmd = 'python -m nuitka --follow-imports --standalone --show-progress --enable-plugin=pyside6  --windows-icon-from-ico=icon.ico --include-package=sqlalchemy,sqlalchemy_codegen  start.py'
 
-# ui文件转py文件
-cmd2 = 'pyside6-uic app/ui/MainWindow.ui -o app/ui/MainWindow.py'
 
-# cx_Freeze打包
-cmd4 = 'pip install cx_Freeze idna'
-cmd3 = 'python setup.py build'
+def nuitaka_pack():
+    # nuitka打包
+    cmd = 'python -m nuitka --follow-imports --standalone --show-progress --enable-plugin=pyside6  --windows-icon-from-ico=icon.ico --include-package=sqlalchemy,sqlalchemy_codegen  start.py'
+    os.system(cmd)
 
-os.popen()
+
+def cx_Freeze_pack():
+    # cx_Freeze方式打包
+    cmd1 = 'pip install cx_Freeze idna'
+    cmd2 = 'python setup.py build'
+    os.system(cmd1)
+    os.system(cmd2)
+
+
+def convert_ui_to_py():
+    # ui文件转py文件
+    cmd = 'pyside6-uic app/ui/MainWindow.ui -o app/ui/MainWindow.py'
+    os.system(cmd)
+
+
+if __name__ == "__main__":
+    pass
+    # nuitaka_pack()
