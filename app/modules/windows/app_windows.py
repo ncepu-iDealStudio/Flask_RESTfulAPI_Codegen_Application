21c3b5909ca16d5bf9b11875185c7449836c460d
jackiex <xjg@ncepu.edu.cn>
2023-02-28 11:58:07 +0800

    修改文件命名

--- app/modules/windows/app_windows.py
+++ app/modules/windows/app_windows.py
@@ -10,12 +10,12 @@ this is function description
 '''
 from PySide6.QtWidgets import QMainWindow
 from .window_main import MainWindow
-from .dialog_loading import DialogLoading
+from .windows_dialog import Dialog
 
 
 class AppWindows(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.main_window = MainWindow()
-        self.main_window.dialog_loading = DialogLoading()
+        self.main_window.dialog_loading = Dialog()
         self.main_window.window_init()
